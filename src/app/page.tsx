0af7b523ce495e3f62ff75a05caa6cb84335b428
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-04-19 13:00:29 +0700

    🆙page.tsx: usage unlock comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Clients from "@/components/Clients";
 import Community from "@/components/Community";
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
+import Unlock from "@/components/Unlock";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <HeroSection />
       <Clients />
       <Community />
+      <Unlock />
     </div>
   );
 }
