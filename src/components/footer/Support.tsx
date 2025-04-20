b4a83209899f9021af562a711910100b1927ca03
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-04-20 20:49:49 +0700

    ⭐create company footer comp

---
+++ src/components/footer/Support.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Support = () => {
+  return (
+    <div className="flex flex-col text gap-2">
+      <h1 className="font-bold">Support</h1>
+
+      <ul>
+        <li>Help Center</li>
+        <li>Terms of service</li>
+        <li>Legal</li>
+        <li>Privacy policy</li>
+        <li>Status</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Support;
