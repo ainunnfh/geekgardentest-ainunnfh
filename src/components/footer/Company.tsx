044c84630cd3c233f1aa75ca4e352dc98439bfc3
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-04-20 20:50:05 +0700

    ⭐create support component

---
+++ src/components/footer/Company.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Company = () => {
+  return (
+    <div className="flex flex-col text gap-2">
+      <h1 className="font-bold">Company</h1>
+
+      <ul>
+        <li>About us</li>
+        <li>Blog</li>
+        <li>Contact us</li>
+        <li>Pricing</li>
+        <li>Testimonials</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Company;
